ddc3063c2a8f90a7a8c07f30dc7b9728291ee95f
Katp00p <31592914+katp00p@users.noreply.github.com>
2025-08-28 23:53:49 -0400

    Initial commit

---
+++ src/stores/index.js
@@ -0,0 +1,6 @@
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
+
+// later you can pinia.use(...) for plugins, but keep it empty now
+export default pinia
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <div class="p-6 bg-zinc-900 rounded-2xl text-emerald-400 text-xl">
+        Tailwind is working
+    </div>
+</template>
+
+<style scoped></style>
