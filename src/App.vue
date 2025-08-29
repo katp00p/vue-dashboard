290f59d08b7e9a9ce53c6b61c1aa3eb5fb179276
jason <31592914+katp00p@users.noreply.github.com>
2025-08-29 00:49:28 -0400

    chore(design): add main tags to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import NavBar from '@/components/NavBar.vue';
 </script>
 
 <template>
-  <RouterView />
+  <NavBar />
+  <main class="mx-auto max-w-[95%] px-2 pt-6 pb-4 h-[calc(100vh-3.5rem)] flex flex-col" id="main">
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
